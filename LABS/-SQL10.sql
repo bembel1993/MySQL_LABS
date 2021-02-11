683992700e745bc99aab36931d4e0dd6e981d63f
bembel1993 <78043683+bembel1993@users.noreply.github.com>
2021-02-11 21:55:28 +0300

    Add files via upload

--- LABS/-SQL10.sql
+++ LABS/-SQL10.sql
@@ -1,10 +1,38 @@
 --1.	Разработать курсор, который выводит все данные о клиенте.
-select * from customers
+SELECT * FROM CUSTOMERS
+DECLARE  @COMPANY   VARCHAR(50),
+         @CREDIT_LIMIT     INT,
+         @message VARCHAR(80)
+PRINT ' LIST CUSTOMERS'
+DECLARE klient_cursor CURSOR LOCAL FOR
+    SELECT COMPANY, CREDIT_LIMIT
+    FROM CUSTOMERS
+    --WHERE COMPANY='First Corp.'
+    ORDER BY COMPANY, CREDIT_LIMIT
 
+OPEN klient_cursor
+FETCH NEXT FROM klient_cursor INTO @COMPANY, @CREDIT_LIMIT
+WHILE @@FETCH_STATUS=0
+BEGIN
+    SELECT @message='COMPANY '+@COMPANY +' '+ 'CREDIT_LIMIT' +cast(@CREDIT_LIMIT as varchar(80))
+                 
+    PRINT @message
+
+-- переход к следующему клиенту--
+
+    FETCH NEXT FROM klient_cursor 
+      INTO @COMPANY, @CREDIT_LIMIT
+END
+CLOSE klient_cursor
+DEALLOCATE klient_cursor
+
+-------------------------------------------------------------------
+select * from customers
+GO
 DECLARE CURSOR_CUST CURSOR--1
 	FOR SELECT * FROM CUSTOMERS -- КУРСОР СОЗДАЕТСЯ НА ОСНОВЕ ОПЕРАТОРА SELECT--1
 OPEN CURSOR_CUST --ДЛЯ ТОГО ЧТОБЫ ЧИТАТЬ СТРОКИ--2
-
+GO
 WHILE @@FETCH_STATUS = 0
 BEGIN
 FETCH NEXT FROM CURSOR_CUST -- ЧТЕНИЕ ОДНОЙ СТРОКИ ИЗ FOR SELECT--3
@@ -14,9 +42,12 @@ CLOSE CURSOR_CUST -- ЗАКРЫТЬ КУРСОР
 DEALLOCATE CURSOR_CUST -- УДАЛИТЬ КУРСОР
 ------------------------------------------------------
 --ПЕРЕМЕННЫЕ ДЛЯ РАБОТЫ
-DECLARE @cust_num INT 
-DECLARE @company VARCHAR(50) 
-DECLARE @cust_rep INT, @credit_limit INT
+GO
+DECLARE @cust_num AS INT 
+DECLARE @company AS VARCHAR(50) 
+DECLARE @cust_rep AS INT 
+DECLARE @credit_limit AS INT
+GO
 --СОЗДАЕМ КУРСОР
 DECLARE custr CURSOR FOR
 	SELECT cust_num, company, cust_rep, credit_limit FROM customers
@@ -25,7 +56,11 @@ DECLARE custr CURSOR FOR
 --ОСУЩЕСТВОЯЕМ ВЫБОРКУ
 WHILE @@FETCH_STATUS = 0
 BEGIN
-SELECT @cust_num, @company, @cust_rep, @credit_limit
+	DECLARE @cust_num AS INT 
+	DECLARE @company AS VARCHAR(50) 
+	DECLARE @cust_rep AS INT 
+	DECLARE @credit_limit AS INT
+	SELECT @cust_num, @company, @cust_rep, @credit_limit
 FETCH NEXT FROM custr INTO @cust_num, @company, @cust_rep, @credit_limit 
 END
 CLOSE custr
@@ -69,46 +104,124 @@ CLOSE customers_cursor;
 DEALLOCATE customers_cursor; 
 
 --2.	Разработать курсор, который выводит все данные о сотрудниках офисов и их количество.
-select * from salesreps
-DECLARE @empl_num int, @name varchar(15),  @message1 varchar(80),
-    @age int, @rep_office int, @title varchar(10),  @manager int, @quota decimal(8, 2), @sales decimal(8, 2);
-DECLARE salesreps_cursor CURSOR FOR  
-SELECT empl_num, name, age, rep_office, title,  manager, quota, sales FROM salesreps;
+select * from OFFICES
+DECLARE @OFFICE INT,
+		@CITY VARCHAR(20),
+		@REGION VARCHAR(20),
+		@MGR INT,
+		@TARGET INT,
+		@SALES INT,
+		@SEND_TO_DISPLAY VARCHAR(1000);
+
+DECLARE OFFICES_CURSOR CURSOR
+FOR SELECT OFFICE, CITY, REGION, COUNT(MGR), TARGET, SALES 
+FROM OFFICES 
+GROUP BY OFFICE, CITY, REGION, MGR, TARGET, SALES;
+
+OPEN OFFICES_CURSOR;
+
+FETCH FROM OFFICES_CURSOR INTO @OFFICE, @CITY, @REGION, @MGR, @TARGET, @SALES
+WHILE @@FETCH_STATUS = 0 
+	BEGIN 
+		SELECT @SEND_TO_DISPLAY = CAST(@OFFICE AS VARCHAR(20))+'---'+ @CITY+'---'+ @REGION +'---'+ 
+		CAST(@MGR AS VARCHAR(20))+'---'+ CAST(@TARGET AS VARCHAR(200))+'---'+CAST(@SALES AS VARCHAR(200));
+	PRINT @SEND_TO_DISPLAY
+	FETCH FROM OFFICES_CURSOR INTO @OFFICE, @CITY, @REGION, @MGR, @TARGET, @SALES
+END
+CLOSE OFFICES_CURSOR
+DEALLOCATE OFFICES_CURSOR
+-----------------------------------------------------------------------------------------------
+DECLARE @empl_num int, 
+	@name varchar(45),
+    --@age int, 
+	--@rep_office int, 
+	@title varchar(40),
+	--@hire_date INT,
+	--@manager int, 
+	--@quota decimal(9, 2), 
+	@sales decimal(9, 2),
+	@SUM INT, 
+	--@message1 varchar(80),
+	@MESSAGE VARCHAR(1000);
+
+DECLARE salesreps_cursor CURSOR 
+FOR SELECT empl_num, name, title, sales, COUNT(MANAGER) FROM salesreps GROUP BY EMPL_NUM, NAME, TITLE, SALES;
  
 OPEN salesreps_cursor;
  
-FETCH FROM salesreps_cursor INTO @empl_num, @name, @age, @rep_office, @title, @manager, @quota, @sales;
- 
+FETCH FROM salesreps_cursor INTO @empl_num, @name, @title, @sales, @SUM;
 WHILE @@FETCH_STATUS = 0 
 BEGIN 
-   SELECT @message1 = cast(@empl_num as varchar(10))+ ' ' + @name + ' ' + @age+ ' ' + ' ' + @rep_office+ ' '+ @title+ ' '+
-   +@manager+' '+@quota+ cast(@sales as varchar(10));
-   PRINT @message1; 
-   FETCH FROM salesreps_cursor INTO @empl_num, @name, @age, @rep_office, @title, @manager, @quota, @sales;
+	--FETCH FROM salesreps_cursor INTO @empl_num, @name, @age, @rep_office, @title, @hire_date, @manager, @quota, @sales;
+   SELECT @MESSAGE = cast(@empl_num as varchar(100))+ ' ' + @name + ' ' +  @title+ ' '+ cast(@sales as varchar(1000))+' '+ 
+   '--QTY CUST-- '+ CAST(@SUM AS VARCHAR(30));
+   PRINT @MESSAGE; 
+   FETCH FROM salesreps_cursor INTO @empl_num, @name,  @title,  @sales, @SUM;
 END ;
 CLOSE salesreps_cursor; 
 DEALLOCATE salesreps_cursor; 
+GO
+--3.	Разработать локальный курсор, который выводит все сведения о товарах и их среднюю цену.
+SELECT AVG(PRICE) FROM PRODUCTS WHERE MFR_ID = 'ACI'
+SELECT * FROM PRODUCTS
+DECLARE @MFR_ID VARCHAR(20),
+		@PRODUCT_ID VARCHAR(20),
+		@PRICE INT,
+		@SEND_TO_DISPLAY VARCHAR(1000);
 
-3.	Разработать локальный курсор, который выводит все сведения о товарах и их среднюю цену.
+DECLARE PRODUCTS_CURSOR CURSOR
+FOR SELECT DISTINCT MFR_ID, PRODUCT_ID, AVG(PRICE)  
+FROM PRODUCTS 
+GROUP BY MFR_ID, PRODUCT_ID, PRICE;
+
+OPEN PRODUCTS_CURSOR;
+
+FETCH FROM PRODUCTS_CURSOR INTO @MFR_ID, @PRODUCT_ID, @PRICE
+WHILE @@FETCH_STATUS = 0 
+	BEGIN 
+		SELECT @SEND_TO_DISPLAY = @MFR_ID+'---'+ @PRODUCT_ID +'---'+ CAST(AVG(@PRICE) AS VARCHAR(20));
+	--PRINT @SEND_TO_DISPLAY
+	SET @SEND_TO_DISPLAY = @MFR_ID + '---'+@PRODUCT_ID+'---'+(select cast(AVG(price) as VARCHAR(20)) from products);
+	PRINT @SEND_TO_DISPLAY
+	--SELECT CAST(AVG(@PRICE) AS VARCHAR(20)) WHERE @MFR_ID = 'ACI'
+	--PRINT CAST(AVG(@PRICE) AS VARCHAR(20))
+	FETCH FROM PRODUCTS_CURSOR INTO @MFR_ID, @PRODUCT_ID, @PRICE
+END
+
+CLOSE PRODUCTS_CURSOR
+DEALLOCATE PRODUCTS_CURSOR
+
 4.	Разработать глобальный курсор, который выводит сведения о заказах, выполненныъ в 2008 году.
 --5.	Разработать статический курсор, который выводит сведения о покупателях и их заказах.
-DECLARE @cust_num int, @company varchar(15),  @message varchar(80),
+DECLARE 
+	@cust_num int, 
+	@company varchar(15),  
+	@message varchar(80),
+	@cust_rep int,
     @credit_limit decimal(8, 2);
+
 DECLARE customers_cursor CURSOR --STATIC
 FOR  
-SELECT cust_num, company, credit_limit FROM customers;
+SELECT * FROM customers;
 PRINT CAST(@@CURSOR_ROWS  AS VARCHAR(10)) -- @@CURSOR_ROWS
 OPEN customers_cursor;
+
 PRINT CAST(@@CURSOR_ROWS  AS VARCHAR(10))
-FETCH FROM customers_cursor INTO @cust_num, @company, @credit_limit;
+FETCH FROM customers_cursor INTO @cust_num2, @company2, @cust_rep2, @credit_limit2;
 PRINT CAST(@@CURSOR_ROWS  AS VARCHAR(10))
 WHILE @@FETCH_STATUS = 0 
 BEGIN 
-   SELECT @message = cast(@cust_num as varchar(10))+ ' ' + @company + ' '  +
-      cast(@credit_limit as varchar(10));
+DECLARE 
+	@message2 varchar(80),
+	@cust_num2 int, 
+	@company2 varchar(15), 
+	@cust_rep2 int,
+    @credit_limit2 decimal(8, 2);
+   SELECT @message2 = @cust_num2 + ' ' + @company2 + ' ' + @cust_rep2 + ' ' +
+      cast(@credit_limit2 as varchar(10));
  
-   PRINT @message; 
-   FETCH FROM customers_cursor INTO @cust_num, @company, @credit_limit;
+   PRINT @message2; 
+   --FETCH FROM customers_cursor INTO @cust_num2, @company2, @cust_rep2, @credit_limit2;
    PRINT CAST(@@CURSOR_ROWS  AS VARCHAR(10))
 END ;
 CLOSE customers_cursor; 
@@ -118,14 +231,23 @@ PRINT CAST(@@CURSOR_ROWS  AS VARCHAR(10))
 
 --6.	Разработать динамический курсор, который обновляет данные о сотруднике в зависимости от суммы выполненных заказов (поле SALES).
 SELECT * FROM ORDERS
-declare @rep int, @sales decimal(9, 2);
+SELECT * FROM salesreps
+declare 
+	@rep int, 
+	@sales decimal(9, 2);
+
 declare update_cursor cursor local dynamic for
 	select rep, sum(amount) as sales_order from orders
 group by rep;
-	open update_cursor;
-	fetch from update_cursor into @rep, @sales;
-	while @@FETCH_STATUS = 0
-		begin
+	
+open update_cursor;
+
+	--fetch from update_cursor into @rep, @sales;
+	while @@FETCH_STATUS = 0		--КОНТРОЛЬ ДОСТИЖЕНИЯ КОНЦА КУРСОРА - ЕСЛИ 0, ТО ВЫБОРКА ПРОШЛА УСПЕШНО
+begin
+declare 
+	@rep int, 
+	@sales decimal(9, 2);
 			update salesreps set sales = @sales
 	where empl_num=@rep
 	fetch from update_cursor into @rep, @sales;
